f09c5f9bb28d68dec07f5fcb3272e233c511611c
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-11-24 12:52:43 +0100

    start main

--- A2.java
+++ A2.java
@@ -1,5 +1,30 @@
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.File;
+
 public final class A2 {
     public static void main(String[] args) {
 
+        for (String f:args) {
+            Reader in;
+            try {
+                in = new FileReader(new File(f));
+            } catch(Exception e) {
+                fatal(e);
+                return;
+            }
+            tokenize(in);
+        }
+
+    }
+
+
+    public static void tokenize(Reader in) {
+
+    }
+
+    public static void fatal(Exception e) {
+        System.err.println(e);
+        System.exit(1);
     }
 }
