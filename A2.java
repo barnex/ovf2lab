7a91f0549570c3b6dedef6c9e2ce7e2f0bdd59f2
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-11-27 12:33:18 +0100

    start main command a2

--- A2.java
+++ A2.java
@@ -1,7 +1,46 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
 
+// a2 main command.
+// a2 scan files: outputs tokens
+// a2 parse files: outputs ast
 public final class A2 {
+
 	public static void main(String[] args) throws Throwable {
-		for (String f:args) {
+
+		if (args.length < 2) {
+			badUsage();
+		}
+
+		String cmd = args[0];
+		if (cmd.equals("scan")) {
+			mainScan(args);
+		} else if(cmd.equals("parse")) {
+			mainParse(args);
+		} else {
+			badUsage();
+		}
+	}
+
+	// main for scanning files
+	static void mainScan(String[] args) throws Throwable {
+		for (int i=1; i<args.length; i++) {
+			String f = args[i];
+			Reader r = new FileReader(new File(f));
+			Scanner s = new Scanner(f, r);
+			while (s.currentToken.type != Token.EOF) {
+				System.out.println(s.currentToken.pos() + "\t" + Token.typeName(s.currentToken.type) + ":\t" +  s.currentToken.value);
+				s.next();
+			}
+			System.out.println(s.currentToken.pos() + "\t" + Token.typeName(s.currentToken.type) + ":\t" +  s.currentToken.value);
+		}
+	}
+
+	// main for parsing files
+	static void mainParse(String[] args) throws Throwable {
+		for (int i=1; i<args.length; i++) {
+			String f = args[i];
 			Parser p = new Parser();
 			p.parseFile(f);
 			if (p.errors.size() > 0) {
@@ -12,7 +51,12 @@ public final class A2 {
 		}
 	}
 
-	public static void fatal(Exception e) {
+	static void badUsage() {
+		System.err.println("Usage: a2 scan|parse <file>");
+		System.exit(1);
+	}
+
+	static void fatal(Exception e) {
 		System.err.println(e);
 		System.exit(1);
 	}
