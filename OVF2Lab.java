50146f0097c72ee1f06e837001d4dee49f0c4fcb
Arne Vansteenkiste <barnex@gmail.com>
2014-12-10 16:37:10 +0100

    ovf2 cleanup

--- OVF2Lab.java
+++ OVF2Lab.java
@@ -1,7 +1,22 @@
+import a2.Interpreter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 class OVF2Lab {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		Interpreter w = new Interpreter();
 
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+
+		for (;;){
+			String line = stdin.readLine();
+			w.exec(line);
+		}
+	
 	}
+
+
 
 }
